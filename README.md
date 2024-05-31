aa8d6aff23bb062319cc51b588e91dbfbf3aab95
Varga Zoltán <varga.zoltan@ingatlan.com>
2024-05-31 13:36:11 +0200

    Init

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-This is my first test project. 
+# Symfony Onion Skeleton
+
+Ez a projekt egy alap vázat biztosít, ha symfony-ban onion architektúra alatt szeretnél fejleszteni.
+
+## Docker elindítása
+
+```bash
+docker-compose up -d
+```
+
+## Composer függőségek telepítése
+
+```bash
+docker-compose run --rm php composer install
+```
+
+Toolok
+
+Easy coding standard futtatás
+
+```bash
+docker-compose run --rm php composer cs
+```
+
+Phpstan futtatás
+
+```bash
+docker-compose run --rm php composer sa
+```
---
+++ src/Infrastructure/Controller/DefaultController.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Infrastructure\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+//    #[Route('/', name: 'default')]
+//    public function index(): Response
+//    {
+//        // return hello
+//        return $this->json([
+//            'message' => 'Welcome to your new controller!',
+//            'path' => 'src/Infrastructure/Controller/DefaultController.php',
+//        ]);
+//    }
+}
